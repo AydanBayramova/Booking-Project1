b7fc5a47107f34b8ca900461c5e69de1781fe317
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-07-02 14:40:19 +0400

    FlightService interfaces

---
+++ src/main/java/service/FlightService.java
@@ -0,0 +1,27 @@
+package service;
+
+import dto.FlightDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface FlightService {
+    void createFlight(FlightDto flightDto);
+
+    void createFlights(List<FlightDto> flightDtos);
+
+    FlightDto retrieveFlight(Long id);
+
+    List<FlightDto> retrieveAllFlights();
+
+    void removeFlight(Long id);
+
+    void updateFlight(Long id, FlightDto updatedFlightDto);
+
+    List<FlightDto> displayOnlineBoard();
+
+
+    FlightDto searchFlight(String destination, LocalDateTime departureTime);
+
+
+}
